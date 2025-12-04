437bb7e07da268efd2cc5a7cc168021019f1afc5
zanriks <arseniysharnin@gmail.com>
2025-12-04 19:05:41 +0700

    step 7

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,37 +1,22 @@
-from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from rest_framework import serializers
 
-# Одна часть класса определяет какие поля будут сериализовать/ десериализовать.
-# Методы create() и update() позволяют определить как будут обрабатываться данные при создании или обновлении сниппета.
-class SnippetSerializer(serializers.ModelSerializer):
+from .models import Snippet
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
    owner = serializers.ReadOnlyField(source='owner.username')
+   highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
    class Meta:
        model = Snippet
-       fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+       fields = ['url', 'id', 'highlight', 'owner',
+                 'title', 'code', 'linenos', 'language', 'style']
 
-   def create(self, validated_data):
-       """
-       Create and return a new `Snippet` instance, given the validated data.
-       """
-       return Snippet.objects.create(**validated_data)
 
-   def update(self, instance, validated_data):
-       """
-       Update and return an existing `Snippet` instance, given the validated data.
-       """
-       instance.title = validated_data.get('title', instance.title)
-       instance.code = validated_data.get('code', instance.code)
-       instance.linenos = validated_data.get('linenos', instance.linenos)
-       instance.language = validated_data.get('language', instance.language)
-       instance.style = validated_data.get('style', instance.style)
-       instance.save()
-       return instance
-
-class UserSerializer(serializers.ModelSerializer):
-   snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+   snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
    class Meta:
        model = User
-       fields = ['id', 'username', 'snippets']
+       fields = ['url', 'id', 'username', 'snippets']
--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,20 @@
-from rest_framework import generics
 from django.contrib.auth.models import User
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+   return Response({
+       'users': reverse('user-list', request=request, format=format),
+       'snippets': reverse('snippet-list', request=request, format=format)
+   })
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -19,6 +29,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+class SnippetHighlight(generics.GenericAPIView):
+   queryset = Snippet.objects.all()
+   renderer_classes = [renderers.StaticHTMLRenderer]
+
+   def get(self, request, **kwargs):
+       snippet = self.get_object()
+       return Response(snippet.highlighted)
 
 class UserList(generics.ListAPIView):
    queryset = User.objects.all()
