ec11af9c8b9b8b9475d084004a516255fd8c7478
zanriks <arseniysharnin@gmail.com>
2025-12-04 17:59:58 +0700

    step 6

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
@@ -8,8 +8,7 @@ urlpatterns = [
    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
    path('users/', views.UserList.as_view()),
    path('users/<int:pk>/', views.UserDetail.as_view()),
+   path('auth/', include('rest_framework.urls')),
 ]
-
-
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,14 +1,16 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import generics
-
+from rest_framework import permissions
 from .models import Snippet
+from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
    def perform_create(self, serializer):
        serializer.save(owner=self.request.user)
@@ -16,6 +18,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 class UserList(generics.ListAPIView):
    queryset = User.objects.all()
