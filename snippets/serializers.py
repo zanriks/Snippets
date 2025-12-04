4a1bd782be92fa7b1096ac5e4203df146124bb59
zanriks <arseniysharnin@gmail.com>
2025-12-04 17:32:49 +0700

    second commit

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,13 +3,10 @@ from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 # Одна часть класса определяет какие поля будут сериализовать/ десериализовать.
 # Методы create() и update() позволяют определить как будут обрабатываться данные при создании или обновлении сниппета.
-class SnippetSerializer(serializers.Serializer):
-   id = serializers.IntegerField(read_only=True)
-   title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-   code = serializers.CharField(style={'base_template': 'textarea.html'})
-   linenos = serializers.BooleanField(required=False)
-   language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-   style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+class SnippetSerializer(serializers.ModelSerializer):
+   class Meta:
+       model = Snippet
+       fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
 
    def create(self, validated_data):
        """
